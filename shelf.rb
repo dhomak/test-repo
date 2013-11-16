43ae349e6874bb84e6ab59e93943f53fd55c5079
David Homak <xtender@gmail.com>
2013-11-17 01:52:27 +0200

    non-working shelf counter

---
+++ shelf.rb
@@ -0,0 +1,23 @@
+#so, we do have a shelf
+#shelf is: shelf = {}
+#shelf consist of books, book title => volume
+#each pair contains books/volume
+#we need to count total volume to know the shelf size
+#so we need to get integer, 'total_volume'
+
+shelf = {"Sandman"=>88, "Dustman"=>75, "Superman"=>62, "BurningMan"=>88, "Countryman"=>14,}
+
+
+#now we have a list of title: volume
+shelf.each do|title,volume|
+  puts "#{title}: #{volume}"
+end
+
+# now we try and get ONLY VOLUMES and put it in the array
+total_volume = (shelf.select { |title,volume|} )
+
+
+
+
+#shelf.values.reduce(0, &:+)
+
