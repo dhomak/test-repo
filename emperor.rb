ca6dc887b78ae34c7e40d22ea11072588d5e17c2
David Homak <xtender@gmail.com>
2013-11-17 02:16:19 +0200

    emperorring

---
+++ emperor.rb
@@ -0,0 +1,16 @@
+def hail_emperor(name)
+	greet = "All hail the Emperor " + name +"!"
+	return greet
+end
+
+#here's our Emperors
+
+puts hail_emperor("Palpatine") 
+puts hail_emperor("of Dune")
+puts hail_emperor("Julius")
+puts hail_emperor("Cartman")
+
+puts "\n Enter your own Emperor"
+joe = gets.chomp.first.upcase
+puts hail_emperor(joe)
+
